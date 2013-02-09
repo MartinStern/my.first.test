bbb1a600a98555e7434519d735e140422285c19d
MartinStern <martin.stern.dao@gmail.com>
2013-02-09 16:59:40 +0100

    inital commit

---
+++ src/ch/daoinformatik/martinsjavatests/part1/HelloWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.daoinformatik.martinsjavatests.part1;
+
+/**
+ *
+ * @author MartinStern
+ */
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
