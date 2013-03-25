1a55959c9b28021b43c64de6ed5af658e44cffb5
MartinStern <martin.stern.dao@gmail.com>
2013-03-25 17:17:04 +0100

    keine ahnung halt was gemacht :-)

---
+++ README.md
@@ -0,0 +1,3 @@
+# test number xy
+
+For GitHub Windows
---
+++ src/ch/daoinformatik/martinsjavatests/part1/ByeByeWorld.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ch.daoinformatik.martinsjavatests.part1;
+
+/**
+ *
+ * @author MartinStern
+ */
+public class ByeByeWorld {
+    public static void main(String attr[]){
+        System.err.println("Lets rock");
+    }
+    
+}
