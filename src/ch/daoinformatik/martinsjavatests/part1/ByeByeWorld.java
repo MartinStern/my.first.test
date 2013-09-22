ff811491975074ff127b7a33ed0c3972f624d919
MartinStern <martin.stern.dao@gmail.com>
2013-09-22 13:44:33 +0200

    nothing special just playing around

--- src/ch/daoinformatik/martinsjavatests/part1/ByeByeWorld.java
+++ src/ch/daoinformatik/martinsjavatests/part1/ByeByeWorld.java
@@ -11,6 +11,11 @@ package ch.daoinformatik.martinsjavatests.part1;
 public class ByeByeWorld {
     public static void main(String attr[]){
         System.err.println("Lets rock");
+        System.out.println("work sucks, but money rocks");
+        System.err.println("I confused about the order the print lines show up in the log :-(");
+        System.out.println("bla bla");
+        System.out.println("it looks like the system.out and system.err commands don't show always up in correct order");
+        
     }
     
 }
